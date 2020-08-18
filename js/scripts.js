ec0b8ee923673049404a39b607b3dd22fde4cf23
Endermax200113 <47977493+Endermax200113@users.noreply.github.com>
2020-08-18 21:00:49 +0300

    Обновление картинки для соц сетей

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,27 @@
 
 var docWidth = window.innerWidth;
 var docHeight = window.innerHeight;
-var deviceMobile = docWidth <= 425 && docWidth >= 320;
-var deviceTablet = docWidth <= 768 && docWidth > 425;
-var deviceLaptop = docWidth <= 1024 && docWidth > 768;
-var deviceComp = docWidth > 1024;
+var deviceSmall = docWidth < 320;
+var deviceMobile = docWidth <= 480 && docWidth >= 320;
+var deviceTablet = docWidth <= 768 && docWidth > 480;
+var deviceLaptop = docWidth <= 1200 && docWidth > 768;
+var deviceComp = docWidth <= 1920 && docWidth > 1200;
+var device2K = docWidth <= 2560 && docWidth > 1920;
+var device4K = docWidth <= 4096 && docWidth > 2560;
+var deviceBig = docWidth > 4096;
 
 $(document).ready(() => {
-	$(window).bind("resize", () => {
+	$(window).on("resize", () => {
 		docWidth = window.innerWidth;
 		docHeight = window.innerHeight;
-		deviceMobile = docWidth <= 425 && docWidth >= 320;
-		deviceTablet = docWidth <= 768 && docWidth > 425;
-		deviceLaptop = docWidth <= 1024 && docWidth > 768;
-		deviceComp = docWidth > 1024;
+		
+		deviceMobile = docWidth <= 480 && docWidth >= 320;
+		deviceTablet = docWidth <= 768 && docWidth > 480;
+		deviceLaptop = docWidth <= 1200 && docWidth > 768;
+		deviceComp = docWidth <= 1920 && docWidth > 1200;
+		device2K = docWidth <= 2560 && docWidth > 1920;
+		device4K = docWidth <= 4096 && docWidth > 2560;
+		deviceBig = docWidth > 4096;
 	});
 });
 
@@ -23,12 +31,20 @@ function testFunction() {
 }
 
 
+//Построение страницы
+
+$(window).ready(() => {
+	
+});
+
+function addHtml(selecter, html) {
+	return $(selecter).html($(selecter).html() + html);
+}
 //Главный скрипт
 
 var version = "0.0.0";
-var copyrite = "© PskovPages";
 
 $(document).ready(() => {
 	console.log("Версия сайта: " + version);
-	console.log(copyrite);
+	console.info("Сайт только для группы 336-ПО");
 });
